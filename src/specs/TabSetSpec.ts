9771ac463c7cceaf59b18215282c0873736c823b
RathaR <ivan.pecharitsa@gmail.com>
2015-07-28 22:03:57 +0300

    enable other specs

--- src/specs/TabSetSpec.ts
+++ src/specs/TabSetSpec.ts
@@ -1,16 +1,16 @@
-xdescribe('TabSet specs', () => {
+describe('TabSet specs', () => {
     beforeEach(() => {
         browser.get('https://angular-ui.github.io/bootstrap/');
     });
     it('should create TabSet with typed header and content', () => {
 
-        var tabset = new UIBootstrap.TabSet<Core.BaseElement, Core.BaseElement>({
+        var tabSet = new UIBootstrap.TabSet<Core.BaseElement, Core.BaseElement>({
             context: element(by.css('body')),
             locator: by.css('#tabs > div:nth-child(2) > div.col-md-6.show-grid > div > div:nth-child(5)')
         }, Core.BaseElement, Core.BaseElement);
 
-        expect(tabset.content().isDisplayed()).toBeTruthy();
-        expect(tabset.content().getText()).toEqual('Static content');
-        expect(tabset.heading().get(0).isDisplayed()).toBeTruthy();
+        expect(tabSet.content().isDisplayed()).toBeTruthy();
+        expect(tabSet.content().getText()).toEqual('Static content');
+        expect(tabSet.heading().get(0).isDisplayed()).toBeTruthy();
     });
 });
