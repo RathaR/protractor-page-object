ba2bbe6e5abacf20641f372d57d7003d6d4b01f5
RathaR <ivan.pecharitsa@gmail.com>
2015-07-30 04:59:08 +0300

    add demo stub

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,32 @@
-angular.module('app', ['ngRoute']);
-angular.module('app').controller('MainController', function () {
-    console.log('init');
-});
+angular.module('app', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/home', {
+                templateUrl: 'views/home.html'
+            })
+            .when('/demo', {
+                templateUrl: 'views/demo.html'
+            })
+            .when('/demo/base-element', {
+                templateUrl: 'views/base-element.html'
+            })
+            .when('/demo/base-elements-list', {
+                templateUrl: 'views/base-elements-list.html'
+            })
+            .otherwise({
+                redirectTo: '/home'
+            })
+    })
+    .controller('HomeController', function () {
+        console.log('init');
+    })
+    .controller('DemoController', function($scope) {
+        var features = [{
+            text: 'Core.BaseElement',
+            link: 'demo/base-element'
+        }, {
+            text: 'Core.BaseElementsList',
+            link: 'demo/base-elements-list'
+        }];
+        $scope.features = features;
+    });
