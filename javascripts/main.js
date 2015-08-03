55b0c8c641ec3b9c0231268140eab5667f0e435a
RathaR <ivan.pecharitsa@gmail.com>
2015-08-03 14:42:10 +0300

    upd views and routes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,17 +1,16 @@
 angular.module('app', ['ngRoute'])
     .config(function ($routeProvider) {
         $routeProvider
-            .when('/home', {
+            .when('/', {
                 templateUrl: 'views/home.html'
             })
-            .when('/demo', {
-                templateUrl: 'views/demo.html'
+            .when('/docs', {
+                templateUrl: 'views/docs.html'
             })
-            .when('/demo/base-element', {
-                templateUrl: 'views/base-element.html'
-            })
-            .when('/demo/base-elements-list', {
-                templateUrl: 'views/base-elements-list.html'
+            .when('/docs/:id', {
+                templateUrl: function(params) {
+                    return 'views/docs/' + params.id + '.html';
+                }
             })
             .otherwise({
                 redirectTo: '/home'
@@ -20,13 +19,16 @@ angular.module('app', ['ngRoute'])
     .controller('HomeController', function () {
         console.log('init');
     })
-    .controller('DemoController', function($scope) {
+    .controller('DocsController', function($scope) {
         var features = [{
             text: 'Core.BaseElement',
-            link: 'demo/base-element'
+            link: 'base-element'
         }, {
             text: 'Core.BaseElementsList',
-            link: 'demo/base-elements-list'
+            link: 'base-elements-list'
+        }, {
+            text: 'Core.BaseApp',
+            link: 'base-app'
         }];
         $scope.features = features;
     });
